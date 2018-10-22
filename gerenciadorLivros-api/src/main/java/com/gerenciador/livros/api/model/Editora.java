dac3b0e08c1033f6cfb56a8a11629ef1af25cdfa
NJunior <NJunior@MP384936.ad.mpsc.mp.br>
2018-10-22 19:10:26 -0200

    ajuste

--- gerenciadorLivros-api/src/main/java/com/gerenciador/livros/api/model/Editora.java
+++ gerenciadorLivros-api/src/main/java/com/gerenciador/livros/api/model/Editora.java
@@ -5,8 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CNPJ;
 
 @Entity
 @Table(name = "editora")
@@ -17,12 +20,47 @@ public class Editora {
 	private Long id;
 
 	@NotNull
-	@Size(min = 3, max = 100)
-	private String nmEditora;
+	@Size(min = 3, max = 50)
+	private String nmFantasia;
 
-	@Size(max = 1000)
+	@NotNull
+	@Size(min = 3, max = 50)
+	private String nmRazaoSocial;
+
+	@NotNull
+	@Max(18)
+	@CNPJ
+	private String nuCnpj;
+
+//	@NotNull
+//	@Size(min = 6, max = 15)
+	private String nuInscricaoEstadual;
+	
+//	@Size(min = 3, max = 20)
+	private String nmContato;
+
+//	@Size(max = 50)
+//	@Email()
+	private String nmEmail;
+
+//	@Size(max = 50)
+	private String nmSite;
+
+//	@Size(max = 15)
+	private String nuFonePrincipal;
+
+//	@Size(max = 15)
+	private String nuFoneSecundario;
+
+//	@Size(max = 15)
+	private String nuFoneCelular;
+
+	private Double vlDesconto;
+
+//	@Max(1000)
 	private String deObservacao;
-
+	
+//	@NotNull
 	private Boolean flForaUso;
 
 	public Long getId() {
@@ -33,12 +71,92 @@ public class Editora {
 		this.id = id;
 	}
 
-	public String getNmEditora() {
-		return nmEditora;
+	public String getNmFantasia() {
+		return nmFantasia;
 	}
 
-	public void setNmEditora(String nmEditora) {
-		this.nmEditora = nmEditora;
+	public void setNmFantasia(String nmFantasia) {
+		this.nmFantasia = nmFantasia;
+	}
+
+	public String getNmRazaoSocial() {
+		return nmRazaoSocial;
+	}
+
+	public void setNmRazaoSocial(String nmRazaoSocial) {
+		this.nmRazaoSocial = nmRazaoSocial;
+	}
+
+	public String getNuCnpj() {
+		return nuCnpj;
+	}
+
+	public void setNuCnpj(String nuCnpj) {
+		this.nuCnpj = nuCnpj;
+	}
+
+	public String getNuInscricaoEstadual() {
+		return nuInscricaoEstadual;
+	}
+
+	public void setNuInscricaoEstadual(String nuInscricaoEstadual) {
+		this.nuInscricaoEstadual = nuInscricaoEstadual;
+	}
+
+	public String getNmContato() {
+		return nmContato;
+	}
+
+	public void setNmContato(String nmContato) {
+		this.nmContato = nmContato;
+	}
+
+	public String getNmEmail() {
+		return nmEmail;
+	}
+
+	public void setNmEmail(String nmEmail) {
+		this.nmEmail = nmEmail;
+	}
+
+	public String getNmSite() {
+		return nmSite;
+	}
+
+	public void setNmSite(String nmSite) {
+		this.nmSite = nmSite;
+	}
+
+	public String getNuFonePrincipal() {
+		return nuFonePrincipal;
+	}
+
+	public void setNuFonePrincipal(String nuFonePrincipal) {
+		this.nuFonePrincipal = nuFonePrincipal;
+	}
+
+	public String getNuFoneSecundario() {
+		return nuFoneSecundario;
+	}
+
+	public void setNuFoneSecundario(String nuFoneSecundario) {
+		this.nuFoneSecundario = nuFoneSecundario;
+	}
+
+	public String getNuFoneCelular() {
+		return nuFoneCelular;
+	}
+
+	public void setNuFoneCelular(String nuFoneCelular) {
+		this.nuFoneCelular = nuFoneCelular;
+	}
+
+	public Double getVlDesconto() {
+		return vlDesconto;
+	}
+
+	public void setVlDesconto(Double vlDesconto) {
+		this.vlDesconto = vlDesconto;
 	}
 
 	public String getDeObservacao() {
@@ -49,7 +167,7 @@ public class Editora {
 		this.deObservacao = deObservacao;
 	}
 
-	public Boolean isFlForaUso() {
+	public Boolean getFlForaUso() {
 		return flForaUso;
 	}
 
--- gerenciadorLivros-api/src/main/resources/db/migration/V001__criar_editoras.sql
+++ gerenciadorLivros-api/src/main/resources/db/migration/V001__criar_editoras.sql
@@ -1,10 +1,20 @@
 CREATE TABLE editora (
 	id BIGINT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	nmEditora VARCHAR(100) NOT NULL,
+	nmFantasia VARCHAR(50) NOT NULL,
+	nmRazaoSocial VARCHAR(50) NOT NULL,
+	nuCnpj VARCHAR(18) NOT NULL,
+	nuInscricaoEstadual VARCHAR(15) NOT NULL,
+	nmContato VARCHAR(20) NULL,
+	nmEmail VARCHAR(50) NULL,
+	nmSite VARCHAR(50) NULL,
+	nuFonePrincipal VARCHAR(15) NULL,
+	nuFoneSecundario VARCHAR(15) NULL,
+	nuFoneCelular VARCHAR(15) NULL,
+	vlDesconto DECIMAL(5,2) NOT NULL,
 	deObservacao VARCHAR(1000) NULL,
 	flForaUso BOOLEAN NOT NULL DEFAULT false
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO editora (nmEditora,flForaUso) VALUES ('Editora Belas Letras',false);
-INSERT INTO editora (nmEditora,flForaUso) VALUES ('Editora Hedra',false);
-INSERT INTO editora (nmEditora,flForaUso) VALUES ('Editora Papa Livros',false);
+INSERT INTO editora 
+(nmFantasia            ,nmRazaoSocial              ,nuCnpj              ,nuInscricaoEstadual,nmContato      ,nmEmail                  ,nmSite            ,nuFonePrincipal,nuFoneSecundario,nuFoneCelular   ,vlDesconto,deObservacao      ,flForaUso) VALUES
+('Editora Belas Letras','Editora Belas Letras LTDA','06.947.994/0001-34','ISENTO'           ,'Nildo Beppler','nildo.beppler@gmail.com','www.nildo.com.br',''             ,''              ,'(48)98466-9011',50.00     ,'Editora do Nidão',false    );
